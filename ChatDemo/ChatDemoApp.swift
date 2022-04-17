6278ace6ea7635eac613782c88fdc6e8ecbb769d
Artem Paliutin <pal.artem@icloud.com>
2022-04-17 15:11:43 +0300

    Initial Commit

---
+++ ChatDemo/ChatDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatDemoApp.swift
+//  ChatDemo
+//
+//  Created by Artem Paliutin on 17/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct ChatDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
